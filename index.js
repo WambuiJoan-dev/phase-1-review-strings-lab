a8e528b54ca40b6feaee7fb7632d93710d4ce8f4
+ <+>
2024-11-25 22:16:50 +0300

    js commit

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser = 'Joan';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//const excitedWelcomeMessage = `WELCOME TO FLATBOOK, JOAN!`;
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "+ currentUser.toUpperCase() + "!"
+const shortGreeting = 'Welcome, J!';
